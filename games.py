b25df9c6e3d2c226b1a7f56253e279cf1ed6793f
Filax <fls.francesco@gmail.com>
2020-03-13 17:11:25 +0100

    Cleaned gamez urls

--- games.py
+++ games.py
@@ -1,15 +1,15 @@
 games_dict_list =\
-[       { 'name' : 'DarkSoulsRem  ', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=3391891997324&categoryName=playstation4-software&superCatName=gaming&title=dark-souls-remastered' },
-        { 'name' : 'DarkSouls2    ', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=3391891983143&categoryName=playstation4-software&superCatName=gaming&title=dark-souls-ii-%282%29-scholar-of-the-first-sin' },
-        { 'name' : 'DarkSouls3    ', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=3391891987332&categoryName=playstation4-software&superCatName=gaming&title=dark-souls-iii' },
-        { 'name' : 'TheSurge      ', 'buy_price' :  5, 'url' : 'https://ie.webuy.com/product-detail?id=3512899117204&categoryName=playstation4-software&superCatName=gaming&title=surge-the' },
-        { 'name' : 'Bloodborne    ', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=711719469919&categoryName=playstation4-software&superCatName=gaming&title=bloodborne' },
-        { 'name' : 'SouthParkStick', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=3307216043270&categoryName=playstation4-software&superCatName=gaming&title=south-park-the-stick-of-truth' },
-        { 'name' : 'SouthParkFract', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=5050013393304&categoryName=playstation4-software&superCatName=gaming&title=south-park-the-fractured-but-whole-%28no-dlc%29' },
-        { 'name' : 'OverCooked2   ', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=5056208800510&categoryName=playstation4-software&superCatName=gaming&title=overcooked-2' },
-        { 'name' : 'HorizonZero   ', 'buy_price' :  5, 'url' : 'https://ie.webuy.com/product-detail?id=711719815341&categoryName=playstation4-software&superCatName=gaming&title=horizon-zero-dawn' },
-        { 'name' : 'TheWitcher3   ', 'buy_price' :  5, 'url' : 'https://ie.webuy.com/product-detail?id=3391891978989&categoryName=playstation4-software&superCatName=gaming&title=witcher-3-wild-hunt' },
-        { 'name' : 'Journey       ', 'buy_price' :  5, 'url' : 'https://ie.webuy.com/product-detail?id=711719841432&categoryName=playstation4-software&superCatName=gaming&title=journey-collectors-edition' },
-        { 'name' : 'DeusEx        ', 'buy_price' :  4, 'url' : 'https://ie.webuy.com/product-detail?id=5021290068995&categoryName=playstation4-software&superCatName=gaming&title=deus-ex-mankind-divided' },
-        { 'name' : 'Destiny1      ', 'buy_price' :  5, 'url' : 'https://ie.webuy.com/product-detail?id=5030917124419&categoryName=playstation4-software&superCatName=gaming&title=destiny' }
+[       { 'name' : 'DarkSoulsRem  ', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=3391891997324' },
+        { 'name' : 'DarkSouls2    ', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=3391891983143' },
+        { 'name' : 'DarkSouls3    ', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=3391891987332' },
+        { 'name' : 'TheSurge      ', 'buy_price' :  5, 'url' : 'https://ie.webuy.com/product-detail?id=3512899117204' },
+        { 'name' : 'Bloodborne    ', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=711719469919' },
+        { 'name' : 'SouthParkStick', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=3307216043270' },
+        { 'name' : 'SouthParkFract', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=5050013393304' },
+        { 'name' : 'OverCooked2   ', 'buy_price' : 15, 'url' : 'https://ie.webuy.com/product-detail?id=5056208800510' },
+        { 'name' : 'HorizonZero   ', 'buy_price' :  5, 'url' : 'https://ie.webuy.com/product-detail?id=711719815341' },
+        { 'name' : 'TheWitcher3   ', 'buy_price' :  5, 'url' : 'https://ie.webuy.com/product-detail?id=3391891978989' },
+        { 'name' : 'Journey       ', 'buy_price' :  5, 'url' : 'https://ie.webuy.com/product-detail?id=711719841432' },
+        { 'name' : 'DeusEx        ', 'buy_price' :  4, 'url' : 'https://ie.webuy.com/product-detail?id=5021290068995' },
+        { 'name' : 'Destiny1      ', 'buy_price' :  5, 'url' : 'https://ie.webuy.com/product-detail?id=5030917124419' }
 ]
